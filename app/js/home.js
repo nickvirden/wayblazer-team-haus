2e7fbfc421f5e9a45a30f0540cf74c43bd99cfe5
Ryan Waits <ryan.waits@gmail.com>
2016-10-01 17:32:00 -0500

    make input results dynamic

--- app/js/home.js
+++ app/js/home.js
@@ -5,17 +5,20 @@ $(function() {
      'x-api-key': '5iR9qzkC5JEvaHT5ILvR6xeymFBQpCV2' 
   };
 
-  $.ajax({
-    url: "https://api.wayblazer.com/v1/typeahead?search=aus",
-    headers: headers
-  }).done(function(data){
-    $.typeahead({
-      input: ".js-typeahead-country_v1",
-      order: "desc",
-      source: {
-        data: data
-      }
-    });
+  $('.js-typeahead-country_v1').on('keyup', function(){
+    var input = $(this).val();
+    $.ajax({
+      url: "https://api.wayblazer.com/v1/typeahead?search=" + input,
+      headers: headers
+    }).done(function(data){
+      $.typeahead({
+        input: ".js-typeahead-country_v1",
+        order: "desc",
+        source: {
+          data: data
+        }
+      });
+    })
   })
 
 
