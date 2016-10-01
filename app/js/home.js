4c0b3358946d5ec825189bf3f9d9fd2e26bad402
Nick Virden <nickvirden@gmail.com>
2016-10-01 23:52:29 +0000

    Orange you Glad I'm updated?

--- app/js/home.js
+++ app/js/home.js
@@ -2,29 +2,27 @@ $(function() {
     
     $(".button-collapse").sideNav();
 
-  var headers = { 
-     'cache-control': 'no-cache',
-     'x-api-key': '5iR9qzkC5JEvaHT5ILvR6xeymFBQpCV2' 
-  };
+    var headers = { 
+        'cache-control': 'no-cache',
+        'x-api-key': '5iR9qzkC5JEvaHT5ILvR6xeymFBQpCV2' 
+    };
 
-  $('.js-typeahead-country_v1').on('keyup', function(){
-    var input = $(this).val();
-    $.ajax({
-      url: "https://api.wayblazer.com/v1/typeahead?search=" + input,
-      headers: headers
-    }).done(function(data){
-      $.typeahead({
-        input: ".js-typeahead-country_v1",
-        order: "desc",
-        source: {
-          data: data
-        }
-      });
-    })
-  })
-
-
-
- 
+    $('.js-typeahead-country_v1').on('keyup', function() {
+        var input = $(this).val();
+        $.ajax({
+            url: "https://api.wayblazer.com/v1/typeahead?search=" + input,
+            headers: headers
+            
+        }).done(function(data){
+            
+            $.typeahead({
+                input: ".js-typeahead-country_v1",
+                order: "desc",
+                source: {
+                    data: data
+                }
+            });
+        })
+    });
     
 });
